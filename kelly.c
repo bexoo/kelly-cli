85cd40a6b71dcd58175639d72e32c6d9575ff7db
be <bradyallen67@gmail.com>
2024-11-17 15:54:50 -0500

    rewrite kelly.c

--- kelly.c
+++ kelly.c
@@ -2,110 +2,89 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct {
+  double bankroll;
+  double win_prob;
+  double gain_ratio;
+} KellyParams;
+
+double parse_double(const char *str, double min, double max) {
+  double val = atof(str);
+  return (val >= min && (max < 0 || val <= max)) ? val : -1.0;
+}
+
+double calc_gain_ratio(double pb, double ob, double bb, double gb) {
+  if (pb > 0)
+    return 1.0 / pb - 1.0;
+  if (ob > 0)
+    return 1.0 / ob;
+  if (bb > 1)
+    return bb - 1.0;
+  return gb;
+}
+
+KellyParams parse_args(int argc, char *argv[]) {
+  KellyParams params = {-1.0, -1.0, -1.0};
+  double pb = -1.0, ob = -1.0, bb = -1.0, gb = -1.0, odds = -1.0;
+
+  for (int i = 1; i < argc; i += 2) {
+    if (i + 1 >= argc)
+      break;
+
+    const char *flag = argv[i];
+    const char *value = argv[i + 1];
+
+    if (strcmp(flag, "-b") == 0)
+      params.bankroll = parse_double(value, 0, -1);
+    else if (strcmp(flag, "-pw") == 0)
+      params.win_prob = parse_double(value, 0, 1);
+    else if (strcmp(flag, "-ow") == 0)
+      odds = parse_double(value, 0, -1);
+    else if (strcmp(flag, "-pb") == 0)
+      pb = parse_double(value, 0, 1);
+    else if (strcmp(flag, "-ob") == 0)
+      ob = parse_double(value, 0, -1);
+    else if (strcmp(flag, "-bb") == 0)
+      bb = parse_double(value, 1, -1);
+    else if (strcmp(flag, "-gb") == 0)
+      gb = parse_double(value, 0, -1);
+  }
+
+  // Convert odds to probability if needed
+  if (params.win_prob < 0 && odds > 0)
+    params.win_prob = odds / (1.0 + odds);
+
+  // Calculate gain ratio from provided parameters
+  int gain_params = (pb > 0) + (ob > 0) + (bb > 0) + (gb > 0);
+  if (gain_params == 1)
+    params.gain_ratio = calc_gain_ratio(pb, ob, bb, gb);
+
+  return params;
+}
+
 int main(int argc, char *argv[]) {
-  double b = -1.0;  // Current bankroll
-  double pw = -1.0; // Probability of winning
-  double ow = -1.0; // Odds of winning
-  double pb = -1.0; // Implied probability given by bet
-  double ob = -1.0; // Implied odds given by bet
-  double bb = -1.0; // Proportion of bet you end with on win
-  double gb = -1.0; // Proprtion of bet you gain on win
-
-  for (int i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "-b") == 0) {
-      b = atof(argv[i + 1]);
-      if (b < 0) {
-        printf("Bankroll must be greater than or equal to 0\n");
-        return 1;
-      }
-    } else if (strcmp(argv[i], "-pw") == 0) {
-      pw = atof(argv[i + 1]);
-      if (!(pw >= 0 && pw <= 1)) {
-        printf("Probability of winning must be between 0 and 1\n");
-        return 1;
-      }
-    } else if (strcmp(argv[i], "-ow") == 0) {
-      ow = atof(argv[i + 1]);
-      if (ow < 0) {
-        printf("Odds of winning must be greater than 0\n");
-        return 1;
-      }
-    } else if (strcmp(argv[i], "-pb") == 0) {
-      pb = atof(argv[i + 1]);
-      if (!(pb > 0 && pb <= 1)) {
-        printf("Implied probability must be between 0 and 1\n");
-        return 1;
-      }
-    } else if (strcmp(argv[i], "-ob") == 0) {
-      ob = atof(argv[i + 1]);
-      if (ob <= 0) {
-        printf("Bet odds must be greater than 0\n");
-        return 1;
-      }
-    } else if (strcmp(argv[i], "-bb") == 0) {
-      bb = atof(argv[i + 1]);
-      if (bb <= 1) {
-        printf("Bet back must be greater than 1\n");
-        return 1;
-      }
-    } else if (strcmp(argv[i], "-gb") == 0) {
-      gb = atof(argv[i + 1]);
-      if (gb <= 0) {
-        printf("Bet gained must be greater than 0\n");
-        return 1;
-      }
-    }
-  }
-  if (b == -1.0 && pw == -1.0 && ow == -1.0 && pb == -1.0 && ob == -1.0 &&
-      bb == -1.0 && gb == -1.0) {
-    printf("Usage: kelly -b <bankroll> -pw <probability of winning> OR -ow "
-           "<odds of winning> -pb <implied probability> OR -ob <implied odds "
-           "of bet> "
-           "OR -bb <proportion of bet you end with on win> OR -gb <proportion "
-           "of bet gained on win>\n");
+  if (argc < 4) {
+    printf(
+        "Usage: kelly -b <bankroll> (-pw <win probability> | -ow <win odds>) "
+        "(-pb <implied probability> | -ob <implied odds> | -bb <winnings to "
+        "bet ratio> | -gb <gained to bet ratio>)\n");
     return 1;
   }
 
-  int count = 0;
-  if (pb != -1.0)
-    count++;
-  if (ob != -1.0)
-    count++;
-  if (bb != -1.0)
-    count++;
-  if (gb != -1.0)
-    count++;
+  KellyParams params = parse_args(argc, argv);
 
-  if (count > 1) {
-    printf("You can only provide one of -pb or -ob or -bb or -gb\n");
-    return 1;
-  } else if (count == 0) {
-    printf("You must provide one of -pb or -ob or -bb or -gb\n");
+  if (params.win_prob < 0 || params.gain_ratio < 0) {
+    printf("Invalid or missing parameters\n");
     return 1;
   }
-  if (pw != -1.0 && ow != -1.0) {
-    printf("You can only provide one of -pw or -ow\n");
-    return 1;
-  }
-  if (pw == -1.0 && ow == -1.0) {
-    printf("You must provide one of -pw or -ow\n");
-    return 1;
-  }
-  if (ow != -1.0) {
-    pw = ow / (1.0 + ow);
-  }
-  if (pb != -1.0) {
-    gb = 1.0 / pb - 1.0;
-  } else if (ob != -1.0) {
-    gb = 1.0 / ob;
-  } else if (bb != -1.0) {
-    gb = bb - 1.0;
-  }
-  double f = pw - ((1 - pw) / gb);
-  if (b == -1.0) {
-    printf("Optimal bet fraction: %.4f\n", f);
-  } else {
-    printf("Optimal bet amount: %.4f\n", f * b);
-  }
+
+  double fraction =
+      params.win_prob - ((1.0 - params.win_prob) / params.gain_ratio);
+
+  if (params.bankroll < 0)
+    printf("Optimal bet fraction: %.4f\n", fraction);
+  else
+    printf("Optimal bet amount: %.4f\n", fraction * params.bankroll);
+
   return 0;
 }
